0b2c8a85d70e89de38f80e116d2565c5a5aab575
geongun20 <leesg9@snu.ac.kr>
2024-04-01 10:07:25 +0900

    fix: use Suspense to fix useSearchParams error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,40 +2,42 @@ import HomeLink from "@/components/HomeLink";
 import AutoRefresh from "./AutoRefresh";
 import { serif } from "@/components/fonts";
 import "./global.css";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import Link from "@/components/Link";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <AutoRefresh>
-      <html lang="en" className={serif.className}>
-        <body className="mx-auto max-w-2xl bg-[--bg] px-5 py-12 text-[--text]">
-          <header className="mb-14 flex flex-row place-content-between">
-            <HomeLink />
-          </header>
-          <main>{children}</main>
-          <footer className="mt-14 flex flex-row gap-x-4">
-            <Link
-              href="mailto://leesg7975@gmail.com"
-              className="text-[13px] underline"
-            >
-              leesg7975@gmail.com
-            </Link>
-            <Link
-              href="https://www.linkedin.com/in/lee-seung-geon/"
-              className="text-[13px] underline"
-            >
-              linkedin
-            </Link>
-            <Link
-              href="https://github.com/geongun20"
-              className="text-[13px] underline"
-            >
-              github
-            </Link>
-          </footer>
-        </body>
-      </html>
+      <Suspense>
+        <html lang="en" className={serif.className}>
+          <body className="mx-auto max-w-2xl bg-[--bg] px-5 py-12 text-[--text]">
+            <header className="mb-14 flex flex-row place-content-between">
+              <HomeLink />
+            </header>
+            <main>{children}</main>
+            <footer className="mt-14 flex flex-row gap-x-4">
+              <Link
+                href="mailto://leesg7975@gmail.com"
+                className="text-[13px] underline"
+              >
+                leesg7975@gmail.com
+              </Link>
+              <Link
+                href="https://www.linkedin.com/in/lee-seung-geon/"
+                className="text-[13px] underline"
+              >
+                linkedin
+              </Link>
+              <Link
+                href="https://github.com/geongun20"
+                className="text-[13px] underline"
+              >
+                github
+              </Link>
+            </footer>
+          </body>
+        </html>
+      </Suspense>
     </AutoRefresh>
   );
 }
